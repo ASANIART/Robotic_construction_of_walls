74a1347f78a7d6b4b022ee55209404a5fdbc601a
ASANIART <39029426+ASANIART@users.noreply.github.com>
2022-06-04 18:18:31 +0200

    Add files via upload

---
+++ Crane_symbolic_equations/gravity_crane.m
@@ -0,0 +1,10 @@
+ 
+G =
+ 
+g*ml6*(d*cos(th2)*sin(th1) - l*cos(th1)*sin(th5) + l*cos(th2)*cos(th4)*cos(th5)*sin(th1) - l*cos(th5)*sin(th1)*sin(th2)*sin(th4))
+                                        g*ml6*cos(th1)*(d*sin(th2) + l*cos(th2)*cos(th5)*sin(th4) + l*cos(th4)*cos(th5)*sin(th2))
+                                                                                                         -g*ml6*cos(th1)*cos(th2)
+                                                                                         g*l*ml6*sin(th2 + th4)*cos(th1)*cos(th5)
+                         -g*l*ml6*(cos(th5)*sin(th1) - cos(th1)*cos(th2)*cos(th4)*sin(th5) + cos(th1)*sin(th2)*sin(th4)*sin(th5))
+                                                                                                                                0
+ 
